8a37330a4d79353b03378e421a1f394843228a4d
carbonsword <ameesh1000@gmail.com>
2020-12-12 20:30:06 -0800

    Update testing.py
    optimized (condensed more if statements into the dictionary)

--- testing.py
+++ testing.py
@@ -8,12 +8,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    
 
 @client.event
 async def on_message(message):
     message.content = message.content.lower()
-    # if message.content.startswith('spam'): 
-    #     await message.channel.send('spam')
+    messageSenderId = '<@' + str(message.mentions[0].id) + '>'
 
     responses = {
     'guys': 'and girls',
@@ -26,7 +26,9 @@ async def on_message(message):
     'is it': 'its not. \nno way... \nwe made it up.\nit\'s a total fabrication',
     'kg': 'is bad',
     '-roastme': 'no one:\nnot even vikram:\n{.author.name}: is a potatohead'.format(message),
-    'no u': 'daaaaamn {.author.name} hit em with dat uno reverse card'.format(message)
+    'no u': 'daaaaamn {.author.name} hit em with dat uno reverse card'.format(message),
+    'girls': 'and guys',
+    '-roast ': ' %s, you are bad ' % messageSenderId
     }
 
     if message.author == client.user:
@@ -35,27 +37,12 @@ async def on_message(message):
     for call in responses:
         if(message.content.startswith(call)):
             await message.channel.send(responses[call])
-
-
-    if message.content.startswith('-roast '):
-        myId = '<@' + str(message.mentions[0].id) + '>'
-        await message.channel.send( ' %s, you are bad ' % myId)
-        # await client.send_message('rE, <!@' + str(message.mentions[0].id) + '> is kindof a bumbo')
-        # await client.send_message(message.channel, 'rE, %s is a bumbo ' % )
         
     if message.content.startswith('-hello'):
         if (message.author.name == 'kgupta_1542'):
             await message.channel.send('shut up kanishk')
         else:
             await message.channel.send('*Hello {.author.name}!*'.format(message))
-
-
-    if message.content.startswith():  
-        await message.channel.send( )
- 
-    # if message.content.startswith('-shut down'):
-    #     await message.channel.send('shutting down')
-    #     await client.logout()
         
 
 
