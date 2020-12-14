edbe0180552f1f5334d605bd88e96837470650b4
carbonsword <ameesh1000@gmail.com>
2020-12-13 17:32:18 -0800

    Update testing.py
    lol made all the responses one line

--- testing.py
+++ testing.py
@@ -19,24 +19,7 @@ async def on_message(message):
         messageSenderId = '<@' + str(message.mentions[0].id) + '>'
         
 
-    responses = {
-    'guys': 'and girls',
-    'why': "because...",
-    'yoink': 'stop yoinken\' the wifi bumbo',
-    'this is': 'is it now?',
-    'shut up': 'rude',
-    'VikBot:': 'righhhhht...',
-    'spam': 'spam-mer? i barely know-her!',
-    'is it': 'its not. \nno way... \nwe made it up.\nit\'s a total fabrication',
-    'kg': 'is bad',
-    '-roastme': 'no one:\nnot even vikram:\n{.author.name}: is a potatohead'.format(message),
-    'no u': 'daaaaamn {.author.name} hit em with dat uno reverse card'.format(message),
-    'girls': 'and guys',
-    '-roast ': ' %s, you are bad ' % messageSenderId,
-    'vikram': 'I am cooler than him :sunglasses:',
-    'kyle': 'kyle *would* be like that',
-    'vc': 'yes',
-    'hmm': 'what?'
+    responses = {'guys': 'and girls', 'why': "because...", 'yoink': 'stop yoinken\' the wifi bumbo', 'this is': 'is it now?', 'shut up': 'rude', 'VikBot:': 'righhhhht...', 'spam': 'spam-mer? i barely know-her!', 'is it': 'its not. \nno way... \nwe made it up.\nit\'s a total fabrication', 'kg': 'is bad', '-roastme': 'no one:\nnot even vikram:\n{.author.name}: is a potatohead'.format(message), 'no u': 'daaaaamn {.author.name} hit em with dat uno reverse card'.format(message), 'girls': 'and guys', '-roast ': ' %s, you are bad ' % messageSenderId, 'vikram': 'I am cooler than him :sunglasses:', 'kyle': 'kyle *would* be like that', 'vc': 'yes', 'hmm': 'what?'
     }
 
     if message.author == client.user:
