bbdc9357aebf44682f7d7369cca10054608f81ee
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2020-12-14 18:29:27 -0800

    Implemented Discord API
    Added a few new responses

--- objOriented.py
+++ objOriented.py
@@ -1,8 +1,13 @@
+import discord
+
+client = discord.Client()
+
+
 class Reply:
-    def __init__(self, message, reply):
+    def __init__(self, message, reply, userDict={}):
         self.message = message
         self.reply = reply
-        self.userDict = {}
+        self.userDict = userDict
 
     def addSpecialReply(self, user, reply):
         self.userDict.update({user: reply})
@@ -16,25 +21,62 @@ class Reply:
         for currUser in self.userDict:
             if currUser == user:
                 finalReply = self.userDict[user]
+                break
 
         return finalReply
 
 
-responseList = [Reply("guys", "and girls"), Reply("why", "because..."), Reply("yoink", "stop yoinken\' the wifi bumbo")]
-
-
 def findReply(message, user):
+    finalReply = ""
+
     for obj in responseList:
-        if obj.message == message:
-            return obj.getReply(user)
+        if message.startswith(obj.message):
+            finalReply = obj.getReply(user)
+            break
+
+    return finalReply
 
 
-print(findReply("-hello", "kgupta1542"))
+responseList = [Reply("guys", "and girls"),
+                Reply("why", "because..."),
+                Reply("yoink", "stop yoinken\' the wifi bumbo"),
+                Reply("-hello", "Hello!", {'kgupta_1542': 'shut up kanishk'}),
+                Reply("this is", "is it now?"),
+                Reply("shut up", "rude"),
+                Reply("VikBot", "righhhhht..."),
+                Reply("spam", "spam-mer? I barely know-her!"),
+                Reply("is it", "its not. \nno way... \nwe made it up.\nit\'s a total fabrication"),
+                Reply("kg", "is the best",
+                      {'marlee noelle': "SUS!"}),
+                Reply("-roastme", "no one:\nnot even vikram:\nameesh: is a potatohead"),
+                Reply("no u", "daaaaamn hit em with dat uno reverse card"),
+                Reply("girls", "and guys"),
+                Reply("-roast", "you are bad"),
+                Reply("vikram", "I am cooler than him :sunglasses:",
+                    {'VikBot': "why are you talking to yourself idiot?",
+                     'ameesh_daryani': "ooh vik and ameesh sitting in a tree. Playing a game together, Rocket League"}),
+                Reply("kyle", "kyle *would* be like that"),
+                Reply("vc", "yes"),
+                Reply("hmm", "what?"),
+                Reply("Bah", "",
+                      {'marlee noelle': "https://tenor.com/view/spongebob-laughing-lol-haha-happy-gif-15812661"}),
+                Reply("gottem", "https://tenor.com/view/ladies-and-gentleman-we-got-him-gif-12313683")]
 
-responseList.append(Reply("-hello","Hello!"))
-responseList[3].addSpecialReply("kgupta1542","shut up kanishk")
-print(findReply("-hello", "kgupta1542"))
 
-responseList[3].removeSpecialReply("kgupta1542")
-print(findReply("-hello", "kgupta1542"))
-print(findReply("why","fakeUser"))
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    message.content = message.content.lower()
+
+    if message.author == client.user:
+        return
+
+    finalReply = findReply(message.content, message.author.name)
+    if len(finalReply) > 0:
+        await message.channel.send(finalReply)
+
+client.run('NzM4NTM0NjYwNDc2NTAyMDI2.XyNUAA.Uw9GF-7oZgd7zzGkvd9nbikiwNc')
