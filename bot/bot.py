535e17aaa213ed98163f26e63e11029ea160a326
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2020-12-16 12:50:43 -0800

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,47 +1,10 @@
 import discord
-import random
 import os
+from reply import Reply
 
 client = discord.Client()
 token = os.getenv("DISCORD_BOT_KEY")
 
-class Reply:
-    def __init__(self, message, reply, frequency=100, searchType=0, inputType=0, userDict={}):
-        self.message = message
-        self.reply = reply
-        self.frequency = frequency
-
-        # 0 -> contains, 1 -> startswith
-        self.searchType = searchType
-
-        # 0 -> none, 1 -> author name, 2 -> author mention, 3 -> message mention
-        self.inputType = inputType
-        self.userDict = userDict
-
-    # Adds a special reply
-    def addSpecialReply(self, user, reply):
-        self.userDict.update({user: reply})
-
-    # Removes a special reply
-    def removeSpecialReply(self, user):
-        self.userDict.pop(user)
-
-    # Returns correct reply given user
-    def getReply(self, user):
-        finalReply = ""
-
-        # Probability test to see if bot sends message
-        if self.frequency > random.randint(1, 100):
-            # Set default message
-            finalReply = self.reply
-
-            # Check for any special messages
-            for currUser in self.userDict:
-                if currUser == user:
-                    finalReply = self.userDict[currUser]
-                    break
-
-        return finalReply
 
 # Finds the correct reply amongst Reply objects
 def findReply(message):
@@ -70,6 +33,7 @@ def findReply(message):
             break
 
     return finalReply
+
 
 # Define all Replies in a list
 responseList = [Reply("guys", "and girls", 50),
@@ -127,7 +91,7 @@ async def on_message(message):
 
     # Find correct reply
     finalReply = findReply(message)
-    
+
     # If reply isn't empty string, send
     if len(finalReply) > 0:
         await message.channel.send(finalReply)
