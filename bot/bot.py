1afd32c65ad7c421b1fa740112c9c143186653ea
carbonsword <ameesh1000@gmail.com>
2021-02-08 16:00:47 -0800

    Update bot.py
    emoji command testing

--- bot/bot.py
+++ bot/bot.py
@@ -66,7 +66,7 @@ responseList = [FuncReply("-meme", printBack, 100),
                 TextReply("spencer", "that koding kid", 50, 0, 0,
                       {"Engineer Zero": "Why you talking to yourself???"}),
                 TextReply("-snack", "https://tenor.com/view/snack-gif-19586327", 75, 1, 0),
-                TextReply("guy", ":gaillou:", 100, 1, 0)]
+                TextReply("guy", ":gaillou: ", 100, 1, 0)]
 
 
 @client.event
